8f2f6cf3df57f7b1688a587b1e1226a7f4a89c3b
Adam Garcia <adamgarcia1994@ucla.edu>
2017-10-30 15:49:07 +0200

    Translations working

--- server/server.js
+++ server/server.js
@@ -8,73 +8,73 @@ var Promise = require('bluebird');
 
 app.get('/', function (req, res) {
 
+	// Pseudocode
+	// 1.  Read text from textfile
+	// 2.  Split text file by word
+	// 3.  Individually translate words via google translate API
+	// 4.  Send back results
+
+	// Holds promises to each translation submission.
 	var wordPromiseArray = [];
-	var wordArray = [];
+
 
 	readFile().then(function(rawText) {
-		console.log('raw text is: ' + rawText);
 
+		// console.log('raw text is: ' + rawText);
 
-		console.log(rawText.split(" "));
+		var rawWordArr = rawText.split(" ");
+		var wordArray = []; // Prepare final storage of words and their translations
 
+		for(var i=0; i<rawWordArr.length; i++) {
 
+			wordPromiseArray.push(
+				indivWord(rawWordArr[i], i)
+					.then(function (word) {
+						wordArray.push(word);
+					})
+			);
 
+		}
 
-
-		wordPromiseArray.push(
-			indivWord("hi")
-				.then(function (word) {
-					wordArray.push(word);
-					// console.log(word);
-					// console.log('hi done');
-				})
-		);
-
-
-		wordPromiseArray.push(
-			indivWord("bye")
-				.then(function (word) {
-					wordArray.push(word);
-				})
-		);
-
+		// Once all async translations have been completed, sort wordArray (via their index) and return to user
 		Promise.all(wordPromiseArray).then(function () {
 			console.log('all done!');
-			res.send(wordArray);
+
+			wordArray.sort(function(a, b) {
+				return a.index - b.index;
+			});
+
+			res.send(wordArray); // Sort async problem?
 		});
-
-
-
-
-
 
 	});
 
 
 });
 
-function indivWord(origWord) {
+// Returns an object with all information necessary to translate a word
+function indivWord(origWord, index) {
 
 	return new Promise(function (resolve) {
 
 		translateWord(origWord)
 			.then(function (response) {
 				resolve({
+					"index": index,
 					"orig": origWord,
 					"trans": response
 				});
 			});
 
-
 	})
 }
 
-
+// Performs actual translation using Google API
 function translateWord(wordToTrans) {
 
 	return new Promise(function (resolve) {
 
-		translate(wordToTrans, {to: 'iw'})
+		translate(wordToTrans, {to: 'en'}) // See http://bit.ly/2zSddBw for list of languages
 			.then(function (res) {
 				console.log(res.text);
 				resolve(res.text);
@@ -85,6 +85,7 @@ function translateWord(wordToTrans) {
 		});
 }
 
+// Function call to read from a file
 function readFile() {
 
 
@@ -95,7 +96,7 @@ function readFile() {
 			console.log('Usage: node ' + process.argv[1] + ' FILENAME');
 			process.exit(1);
 		}
-// Read the file and print its contents.
+		// Read the file and print its contents.
 		var fs = require('fs')
 			, filename = process.argv[2];
 		fs.readFile(filename, 'utf8', function (err, data) {
